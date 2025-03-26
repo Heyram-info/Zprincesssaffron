7a76767d84ad674e0eb794016eab59e260867734
GokulTR-07 <gokultr1206@gmail.com>
2025-03-26 10:58:35 +0530

    vercel file created

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,17 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 import { BrowserRouter as Router } from "react-router-dom";
 import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:4040";
 axios.defaults.withCredentials = true;
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   // <StrictMode>
   //   </StrictMode>
-  <Router>
+    <Router>
     <App />
-  </Router>
-);
+    </Router>
+  
+  ,
+)
--- backend/index.js
+++ backend/index.js
@@ -4,29 +4,16 @@ import authRoutes from './routes/authRoutes.js'
 import {mongoose} from "mongoose";
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
-import jwt from "jsonwebtoken"
-import Product from "./models/product.js";
 
 const app = express();
 dotenv.config();
 
-// const allowedOrigins = ['http://localhost:5173/'];
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if (allowedOrigins.includes(origin) || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true, // Enable credentials
-// }));
-// Allow requests from your Vercel frontend
+
 app.use(cors({
-  origin: 'http://localhost:5173', // Your Vercel domain https://web-1-8cji.vercel.app
+  origin: 'http://localhost:5173', // Your Vercel domain 
   credentials: true
 }));
-// https://saffronfrontend-lq61-2m631zo39-prems-projects-8f178100.vercel.app
+
 // middleware
 app.use(express.json())
 app.use(cookieParser())
